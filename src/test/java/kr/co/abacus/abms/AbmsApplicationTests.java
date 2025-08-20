8639a3b3aaef1626b87e07fcba839da7d03484da
sangcheol <tkdcjf38@iabacus.co.kr>
2025-08-21 00:09:14 +0900

    test: 기본 애플리케이션 실행 테스트 개선

--- src/test/java/kr/co/abacus/abms/AbmsApplicationTests.java
+++ src/test/java/kr/co/abacus/abms/AbmsApplicationTests.java
@@ -1,13 +1,20 @@
 package kr.co.abacus.abms;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AbmsApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void run() {
+        try (MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class)) {
+            AbmsApplication.main(new String[0]);
 
+            mocked.verify(() -> SpringApplication.run(AbmsApplication.class, new String[0]));
+        }
+    }
 }
