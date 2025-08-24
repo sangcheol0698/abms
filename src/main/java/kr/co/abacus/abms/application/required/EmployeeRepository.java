b9a02a4b5391b14bb292fb4466862e0b55baddee
sangcheol <tkdcjf38@iabacus.co.kr>
2025-08-25 00:11:09 +0900

    refactor(repo): EmployeeRepository를 Repository 인터페이스로 변경 및 save 메서드 명시
    EmployeeRepository가 JpaRepository 대신 Repository 인터페이스를 상속하도록 변경하여 필요한 메서드만 명시적으로 정의합니다. save 메서드를 추가하여 명시적으로 사용 가능하도록 합니다.

--- src/main/java/kr/co/abacus/abms/application/required/EmployeeRepository.java
+++ src/main/java/kr/co/abacus/abms/application/required/EmployeeRepository.java
@@ -3,12 +3,14 @@ package kr.co.abacus.abms.application.required;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
 import kr.co.abacus.abms.domain.employee.Employee;
 import kr.co.abacus.abms.domain.shared.Email;
 
-public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+public interface EmployeeRepository extends Repository<Employee, UUID> {
+
+    Employee save(Employee employee);
 
     boolean existsByEmail(Email email);
 
