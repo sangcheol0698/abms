7288cfe04ad06ca0fd8cddb1ba896b3f32c023d0
sangcheol <scpark0698@gmail.com>
2025-09-13 17:19:16 +0900

    refactor: 테스트 코드 불필요 메서드 제거
    - findDeleted 테스트 메서드 삭제

--- src/test/java/kr/co/abacus/abms/application/department/provided/DepartmentFinderTest.java
+++ src/test/java/kr/co/abacus/abms/application/department/provided/DepartmentFinderTest.java
@@ -1,9 +1,7 @@
 package kr.co.abacus.abms.application.department.provided;
 
 import kr.co.abacus.abms.domain.department.Department;
-import kr.co.abacus.abms.domain.department.DepartmentFixture;
 import kr.co.abacus.abms.domain.department.DepartmentNotFoundException;
-import kr.co.abacus.abms.domain.department.DepartmentType;
 import kr.co.abacus.abms.support.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,22 +29,6 @@ class DepartmentFinderTest extends IntegrationTestBase {
     @Test
     void findNotFound() {
         assertThatThrownBy(() -> departmentFinder.find(UUID.randomUUID()))
-            .isInstanceOf(DepartmentNotFoundException.class);
-    }
-
-    @Test
-    void findDeleted() {
-        // 새로운 부서를 만들어서 삭제 테스트 - 고유한 코드 사용
-        Department newDepartment = Department.createRoot(
-            DepartmentFixture.createDepartmentCreateRequest("삭제테스트회사", "DELETE_TEST_COMPANY", DepartmentType.COMPANY)
-        );
-        departmentRepository.save(newDepartment);
-        flush();
-
-        newDepartment.softDelete("testUser");
-        flushAndClear();
-
-        assertThatThrownBy(() -> departmentFinder.find(newDepartment.getId()))
             .isInstanceOf(DepartmentNotFoundException.class);
     }
 
