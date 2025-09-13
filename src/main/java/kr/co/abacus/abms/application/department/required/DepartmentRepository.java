0d6a075ab8f04286cdd93d5eb1f2f2476db7fd58
sangcheol <scpark0698@gmail.com>
2025-09-13 14:49:36 +0900

    refactor: DepartmentRepository 메서드 정리
    - findByCodeAndDeletedFalse 및 findAll 메서드 제거
    - 불필요한 import 삭제

--- src/main/java/kr/co/abacus/abms/application/department/required/DepartmentRepository.java
+++ src/main/java/kr/co/abacus/abms/application/department/required/DepartmentRepository.java
@@ -3,7 +3,6 @@ package kr.co.abacus.abms.application.department.required;
 import kr.co.abacus.abms.domain.department.Department;
 import org.springframework.data.repository.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,10 +11,6 @@ public interface DepartmentRepository extends Repository<Department, UUID> {
     Department save(Department department);
 
     Optional<Department> findByIdAndDeletedFalse(UUID id);
-
-    Optional<Department> findByCodeAndDeletedFalse(String code);
-
-    List<Department> findAll();
 
     boolean existsById(UUID departmentId);
 }
