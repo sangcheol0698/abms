125de1e5be93e28906da071a35df210868cbe521
sangcheol <tkdcjf38@iabacus.co.kr>
2025-09-19 01:24:52 +0900

    feat(application/employee): 리더 조회 메서드 추가(findAllByIdInAndDeletedFalse)

--- src/main/java/kr/co/abacus/abms/application/employee/required/EmployeeRepository.java
+++ src/main/java/kr/co/abacus/abms/application/employee/required/EmployeeRepository.java
@@ -1,12 +1,13 @@
 package kr.co.abacus.abms.application.employee.required;
 
-import kr.co.abacus.abms.domain.employee.Employee;
-import kr.co.abacus.abms.domain.shared.Email;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.repository.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
+import kr.co.abacus.abms.domain.employee.Employee;
+import kr.co.abacus.abms.domain.shared.Email;
 
 public interface EmployeeRepository extends Repository<Employee, UUID> {
 
@@ -19,5 +20,7 @@ public interface EmployeeRepository extends Repository<Employee, UUID> {
     Optional<Employee> findByIdAndDeletedFalse(UUID id);
 
     boolean existsByEmailAndIdNot(Email email, UUID employeeId);
+
+    List<Employee> findAllByIdInAndDeletedFalse(List<UUID> leaderIds);
 
 }
