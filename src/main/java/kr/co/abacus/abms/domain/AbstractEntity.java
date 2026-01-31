f063a18fed6d43b64342f40ac30d243471183170
sangcheol <tkdcjf38@iabacus.co.kr>
2026-02-01 01:28:03 +0900

    refactor: AbstractEntity equals 및 hashCode 메서드 개선
    - `HibernateProxy` 처리 코드에서 타입 캐스팅 구조 간소화
    - 코드 가독성 향상을 위한 줄바꿈 및 조건문 정리
    - 불필요한 import 정리 및 코드 정리

--- src/main/java/kr/co/abacus/abms/domain/AbstractEntity.java
+++ src/main/java/kr/co/abacus/abms/domain/AbstractEntity.java
@@ -1,17 +1,15 @@
 package kr.co.abacus.abms.domain;
 
-import java.util.Objects;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-
+import lombok.Getter;
+import lombok.ToString;
 import org.hibernate.proxy.HibernateProxy;
 import org.jspecify.annotations.Nullable;
 
-import lombok.Getter;
-import lombok.ToString;
+import java.util.Objects;
 
 @ToString
 @MappedSuperclass
@@ -25,26 +23,26 @@ public abstract class AbstractEntity extends BaseEntity {
     @Override
     @SuppressWarnings("EqualsGetClass")
     public final boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null)
-            return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy
-                ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+        if (this == o) return true;
+        if (o == null) return false;
+
+        Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateproxy
+                ? hibernateproxy.getHibernateLazyInitializer().getPersistentClass()
                 : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy
-                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateproxy
+                ? hibernateproxy.getHibernateLazyInitializer().getPersistentClass()
                 : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass)
-            return false;
+
+        if (thisEffectiveClass != oEffectiveClass) return false;
+
         AbstractEntity that = (AbstractEntity) o;
         return getId() != null && Objects.equals(getId(), that.getId());
     }
 
     @Override
     public final int hashCode() {
-        return this instanceof HibernateProxy
-                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+        return this instanceof HibernateProxy hibernateproxy
+                ? hibernateproxy.getHibernateLazyInitializer().getPersistentClass().hashCode()
                 : getClass().hashCode();
     }
 
